b2d1b890e9426a11e7fc6b74f430f3519d99da3c
snepote <snepote@gmail.com>
2022-07-16 16:15:29 +0200

    Turbo frames in action

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,27 +2,23 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    # We need to order quote as well in the system tests
+    @quote = Quote.ordered.first
   end
 
   test "Creating a new quote" do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes"
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
+    click_on "New quote"    
     fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes" # We expect to remain on Quotes#index page    
     click_on "Create quote"
 
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
+    # After the new quote is created
+    # we expect to remain on Quotes#index page
+    # and see the name of the new quote on the same page
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
@@ -39,9 +35,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes" # We expect to remain on Quotes#index page    
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
