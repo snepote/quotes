cecedb2be75e6f680d51571b1da7003e7a93b182
snepote <snepote@gmail.com>
2022-09-12 22:49:02 +0400

    Flash messages with Turbo using a helper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Entry point for the build script in your package.json
 import "./controllers"
-
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+import "@hotwired/turbo-rails"
